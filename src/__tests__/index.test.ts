2d6563faf86949477169e02091728111063ce08e
Noam Gabriel Jacobson <njacobson@developertown.com>
2020-04-06 17:26:21 -0400

    tests: formatted and improved typing.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,12 +2,7 @@ import * as index from "../index";
 
 describe("index", () => {
   it("doesn't over-export", () => {
-    const allowedExports = [
-      "Lottie",
-      "useLottie",
-      "Renderer",
-      "ClickAway"
-    ];
+    const allowedExports = ["Lottie", "useLottie", "Renderer", "ClickAway"];
     expect(Object.keys(index).sort()).toEqual(allowedExports.sort());
   });
 });
