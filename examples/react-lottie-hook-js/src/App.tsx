38602b972f10a2b9c20307aede30039020499403
Noam Jacobson <njacobson@litera.com>
2021-05-10 06:34:00 -0500

    clean up in js example

--- examples/react-lottie-hook-js/src/App.tsx
+++ examples/react-lottie-hook-js/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />f
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
